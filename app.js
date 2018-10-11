c4b5f6f175dfa722c65164cb3aae108e832149fe
bikilone <milosbirovljevic@gmail.com>
2018-10-11 11:05:02 +0200

    basic config

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ var express = require("express"),
     bodyPaser = require("body-parser"),
     mongoose = require("mongoose");
 
-app.listen("3000", process.env.IP, function() {
-    console.log("server is up on 3000")
+// app config
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyPaser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost/blog");
+
+/// mongose model config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    date: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+
+
+// ROUTES 
+
+app.get("/blogs", function(req, res) {
+    res.render("index");
+})
+
+
+
+app.listen("3001", process.env.IP,  function() {
+    console.log("server is up on 3001")
 })
