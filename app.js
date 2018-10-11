69e4d2daec556c7be098297a0a3a71cff595ea8b
bikilone <milosbirovljevic@gmail.com>
2018-10-11 13:27:56 +0200

    PUT method

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require("express"),
     app = express(),
     bodyPaser = require("body-parser"),
     mongoose = require("mongoose");
+methodOverride = require("method-override");
 
 // app config
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyPaser.urlencoded({extended: true}));
+app.use(bodyPaser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 mongoose.connect("mongodb://localhost/blog");
 
@@ -15,7 +17,7 @@ var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    date: {type: Date, default: Date.now}
+    date: { type: Date, default: Date.now }
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
@@ -24,30 +26,30 @@ var Blog = mongoose.model("Blog", blogSchema);
 
 
 // ROUTES 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.redirect("/blogs");
 })
 // INDEX ROUTE
-app.get("/blogs", function(req, res) {
-    Blog.find({}, function(err, success) {
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, success) {
         if (err) {
             console.log("There was an error: ", err);
         } else {
             console.log("All went fine");
-            res.render("index", {blogs: success});
+            res.render("index", { blogs: success });
         }
     })
 });
 // NEW ROUTE
-app.get("/blogs/new", function(req, res) {
+app.get("/blogs/new", function (req, res) {
     res.render("new");
 });
 
 // CREATE ROUTE
 
-app.post("/blogs", function(req, res) {
-    Blog.create(req.body.blog, function(err, success) {
-        if(err) {
+app.post("/blogs", function (req, res) {
+    Blog.create(req.body.blog, function (err, success) {
+        if (err) {
             res.render("new");
         } else {
             res.redirect("/blogs")
@@ -57,19 +59,43 @@ app.post("/blogs", function(req, res) {
 
 // SHOW ROUTE
 
-app.get("/blogs/:id", function(req, res) {
+app.get("/blogs/:id", function (req, res) {
     var id = req.params.id;
-    Blog.findById(id, function(err, success) {
-        if(err) {
+    Blog.findById(id, function (err, success) {
+        if (err) {
             res.redirect("/blogs");
         } else {
-            res.render("show", {blog: success});
+            res.render("show", { blog: success });
+        }
+    })
+});
+
+// EDIT ROUTE
+
+app.get("/blogs/:id/edit", function (req, res) {
+    Blog.findById(req.params.id, function (err, success) {
+        if (err) {
+            res.redirect("/blogs/")
+        } else {
+            res.render("edit", { blog: success });
+        }
+    })
+});
+
+// UPDATE ROUTE
+
+app.put("/blogs/:id", function (req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, success) {
+        if (err) {
+            res.redirect("/blogs")
+        } else {
+            res.redirect("/blogs/" + req.params.id)
         }
     })
 })
 
 
 
-app.listen("3001", process.env.IP,  function() {
+app.listen("3001", process.env.IP, function () {
     console.log("server is up on 3001")
 })
